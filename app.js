f16d29e9707b0f109ed754a2f3880468bd535230
Frank Ramos <frankmrf11@gmail.com>
2025-02-07 01:22:50 -0500

    Seguimos desarrollando el proyecto final

---
+++ app.js
@@ -0,0 +1,10 @@
+//let principalDOM = document.getElementById("principal");
+//let hedDOM = document.getElementById("hed")
+/*hedDOM.innerHTML=`
+    <img src="./images/Frankm.jpg>
+`*/
+const boton = document.getElementById('b1');
+
+boton.addEventListener('click', function(){
+    boton.style.backgroundColor = 'blue';
+});
