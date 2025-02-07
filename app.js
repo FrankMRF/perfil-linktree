9d61dcec20fcb832e97fbabf8d212ea745e4a240
Frank Ramos <frankmrf11@gmail.com>
2025-02-07 17:32:20 -0500

    Proyecto terminado

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-//document.querySelector('h1').textContent='Miguel Flores'
+
 
 let principalDOM = document.getElementById("hed")
 principalDOM.innerHTML=`
     <h1>Frank Ramos</h1>
 `
-
 let secundarioDOM = document.querySelector("#secundario")
 secundarioDOM.innerHTML=`
     <p>Desarrollador junior | Inversionista novato</p>
 `
-let iconosDOM = document.querySelector("#iconos")
-iconosDOM.style.color = 'white';
+let parrafoDOM = document.querySelector("#parrafo")
+parrafoDOM.style.color='pink';
